b246d9c65864c805e3d0b514c6f80ef643ea09f7
abnerdev777 <abnerwil2015@gmail.com>
2024-06-24 03:26:55 -0300

    ass
    asasas

--- src/teste.js
+++ src/teste.js
@@ -5,13 +5,21 @@ require('dotenv/config');
 
 class Pagamento {
   async store(req, res) {
-    const { chave_pix2, tipopix } = req.body;
+    // Captura dos parâmetros da URL (query parameters)
+    const { chave_pix2, tipopix } = req.query;
+
+    // Captura dos parâmetros do corpo da requisição (body)
+    const { chave_pix2_body, tipopix_body } = req.body;
+
+    // Prioriza os parâmetros do corpo se estiverem presentes
+    const chavePix = chave_pix2_body || chave_pix2;
+    const tipoPix = tipopix_body || tipopix;
 
     try {
       // Verificar se a chave PIX já está registrada
       const verification = await Users.findOne({
         where: {
-          usr_pix: chave_pix2,
+          usr_pix: chavePix,
         },
       });
 
@@ -24,8 +32,8 @@ class Pagamento {
         'https://ws.suitpay.app/api/v1/gateway/pix-payment',
         {
           value: 0.1,
-          key: chave_pix2,
-          typeKey: tipopix,
+          key: chavePix,
+          typeKey: tipoPix,
         },
         {
           headers: {
@@ -38,17 +46,16 @@ class Pagamento {
 
       // Criar registro no banco de dados
       const createdata = {
-        usr_pix: chave_pix2,
+        usr_pix: chavePix,
       };
       await Users.create(createdata);
-      console.log('enviado pix');
+      console.log('PIX enviado com sucesso');
 
       // Enviar a resposta ao cliente
       res.status(200).json(response.data);
     } catch (error) {
       const novoErro = error.response ? error.response.data : error.message;
-      console.error('Error:', novoErro);
-      console.log('verificando erro: ', novoErro);
+      console.error('Erro:', novoErro);
 
       // Enviar uma resposta adequada ao cliente
       res.status(500).json({ error: novoErro });
