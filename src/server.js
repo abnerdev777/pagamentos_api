34acad384f2fd0b074af0a9842eed4739ede2bef
abnerdev777 <abnerwil2015@gmail.com>
2024-06-24 01:15:44 -0300

    2
    2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 import app from './app';
 
-export const port_server = 8000;
+export const port_server = 8080;
 
 app.listen(port_server, () => {
   console.log(`Iniciando Backend Port ${port_server}`);
